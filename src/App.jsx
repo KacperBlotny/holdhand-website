528a79ad14203075b8900f9f05386b891447aad1
kacperblotny <blotnykacper@gmail.com>
2024-03-11 21:10:53 +0100

    pre-final2

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 
-import Navbar from "./components/shared/Navbar";
-import Footer from "./components/shared/Footer";
+import Navbar from './components/shared/Navbar'
+import Footer from './components/shared/Footer'
 
-import Index from "./pages/Index";
-import About from "./pages/About";
-import Employees from "./pages/Employees";
-import Employers from "./pages/Employers";
-import Agency from "./pages/Agency";
+import Index from './pages/Index'
+import About from './pages/About'
+import Employees from './pages/Employees'
+import Employers from './pages/Employers'
+import Agency from './pages/Agency'
 
 function App() {
   return (
     <Router>
       <ScrollToTop />
       <Navbar />
-      {/* <div className="container flex mx-auto"> */}
       <div>
         <Routes>
-          <Route path="/" element={<Index />}></Route>
+          <Route path='/' element={<Index />}></Route>
 
-          <Route path="/about-us" element={<About />} />
-          <Route path="/for-employees" element={<Employees />} />
-          <Route path="/for-employers" element={<Employers />} />
-          <Route path="/for-agency" element={<Agency />} />
+          <Route path='/about-us' element={<About />} />
+          <Route path='/for-employees' element={<Employees />} />
+          <Route path='/for-employers' element={<Employers />} />
+          <Route path='/for-agency' element={<Agency />} />
         </Routes>
       </div>
       <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
