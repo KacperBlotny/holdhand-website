651e9509471268d76e0638534b07ccfba9945850
kacperblotny <blotnykacper@gmail.com>
2024-06-25 17:37:47 +0200

    download

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -15,12 +15,19 @@ const DownloadButton = () => {
   }
 
   return (
-    <button
-      onClick={handleDownload}
+    // <button
+    //   onClick={handleDownload}
+    //   className='rounded-3xl flex justify-center py-4 px-8 border border-main text-sm font-medium text-main hover:text-mainHover transition-all duration-300 mt-8'
+    // >
+    //   Pobierz formularz wyceny
+    // </button>
+    <a
+      href='https://we.tl/t-EYaS0J6S3Z'
+      target='_blank'
       className='rounded-3xl flex justify-center py-4 px-8 border border-main text-sm font-medium text-main hover:text-mainHover transition-all duration-300 mt-8'
     >
       Pobierz formularz wyceny
-    </button>
+    </a>
   )
 }
 
--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -111,7 +111,7 @@ function Agency() {
         <br />
 
         <p>
-          Wypełnij poniższy formularz i prześlij na nasz adres emial
+          Wypełnij poniższy formularz i prześlij na nasz adres e-mail
           <span className='font-bold'> office@holdhand.eu</span>, a my
           sporządzimy kompleksową wycenę, którą prześlemy pod twój adres e-mail.
         </p>
